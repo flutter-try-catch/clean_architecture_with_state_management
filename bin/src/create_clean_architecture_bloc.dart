c9f753f2eb1e58a3e273625a6f2ca8a42c52442d
mostafa aboheaba <mostafa.khalile.aboheaba@gmial.com>
2024-02-13 14:25:49 +0200

    fixes of flutter lints for bloc feature

--- bin/src/create_clean_architecture_bloc.dart
+++ bin/src/create_clean_architecture_bloc.dart
@@ -3,178 +3,39 @@ import '../clean_architecture_with_state_management.dart';
 
 Future createCleanArchitectureBlocFiles(String featureName) async {
   await addBlocPackage();
-  createDirectories(featureName);
-  createDataFiles(featureName);
-  createDomainFiles(featureName);
-  createPresentationFiles(featureName);
+
+  addBlocFiles(featureName);
   // createInitParamsFile(featureName);
   createInjectionContainerFile(featureName);
 }
 
-void createDirectories(String featureName) {
-  Directory('lib/features/${featureName.toSnakeCase()}/data/data_sources/local')
-      .createSync(recursive: true);
-  Directory(
-          'lib/features/${featureName.toSnakeCase()}/data/data_sources/remote')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/data/models')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/data/repositories')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/domain/entities')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/domain/repositories')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/domain/use_cases')
-      .createSync(recursive: true);
+// void createDirectories(String featureName) {
+//   Directory('lib/features/${featureName.toSnakeCase()}/data/data_sources/local')
+//       .createSync(recursive: true);
+//   Directory(
+//           'lib/features/${featureName.toSnakeCase()}/data/data_sources/remote')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/data/models')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/data/repositories')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/domain/entities')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/domain/repositories')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/domain/use_cases')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/presentation/bloc')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/presentation/widgets')
+//       .createSync(recursive: true);
+//   Directory('lib/features/${featureName.toSnakeCase()}/presentation/screens')
+//       .createSync(recursive: true);
+// }
+
+void addBlocFiles(String featureName) {
   Directory('lib/features/${featureName.toSnakeCase()}/presentation/bloc')
       .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/presentation/widgets')
-      .createSync(recursive: true);
-  Directory('lib/features/${featureName.toSnakeCase()}/presentation/screens')
-      .createSync(recursive: true);
-}
-
-void createDataFiles(String featureName) {
-  File('lib/features/${featureName.toSnakeCase()}/data/data_sources/local/${featureName.toSnakeCase()}_local_data_source.dart')
-      .writeAsStringSync('''
-import 'package:dartz/dartz.dart';
-
-
-abstract class ${featureName.capitalize()}LocalDataSource {
-  Future<Unit> getFromLocalDataBase();
-}
-
-class ${featureName.capitalize()}LocalDataSourceImpl implements ${featureName.capitalize()}LocalDataSource {
-  ${featureName.capitalize()}LocalDataSourceImpl();
-
-  @override
-  Future<Unit> getFromLocalDataBase() async {
-    // send api request here
-    return Future.value(unit);
-  }
-
-}
-
-
-  ''');
-  File('lib/features/${featureName.toSnakeCase()}/data/data_sources/remote/${featureName.toSnakeCase()}_remote_data_source.dart')
-      .writeAsStringSync('''
-import 'package:dartz/dartz.dart';
-
-
-abstract class ${featureName.capitalize()}RemoteDataSource {
-  Future<Unit> callApi();
-}
-
-class ${featureName.capitalize()}RemoteDataSourceImpl implements ${featureName.capitalize()}RemoteDataSource {
-  ${featureName.capitalize()}RemoteDataSourceImpl();
-
-  @override
-  Future<Unit> callApi() async {
-    // send api request here
-    return Future.value(unit);
-  }
-
-}
-
-
-  ''');
-  File('lib/features/${featureName.toSnakeCase()}/data/models/${featureName.toSnakeCase()}_model.dart')
-      .writeAsStringSync('''
-import '../../domain/entities/${featureName.toSnakeCase()}.dart';
-
-class ${featureName.capitalize()}Model extends ${featureName.capitalize()} {
-  const ${featureName.capitalize()}Model(
-      {required String data})
-      : super(data: data);
-
-  ${featureName.capitalize()}Model copyWith({
-    String? data,
-  }) {
-    return ${featureName.capitalize()}Model(
-      data: data ?? this.data  ?? '',
-    );
-  }
-
-  Map<String, dynamic> toJson() => {
-    "data": data,
-  };
-
-  factory ${featureName.capitalize()}Model.fromJson(Map<String, dynamic> json) => ${featureName.capitalize()}Model(
-    data: json["data"],
-  );
-}
-
-''');
-  File('lib/features/${featureName.toSnakeCase()}/data/repositories/${featureName.toSnakeCase()}_repository_impl.dart')
-      .writeAsStringSync('''
-import 'package:dartz/dartz.dart';
-import '../../domain/repositories/${featureName.toSnakeCase()}_repository.dart';
-import '../data_sources/remote/${featureName.toSnakeCase()}_remote_data_source.dart';
-
-
-class ${featureName.capitalize()}RepositoryImpl implements ${featureName.capitalize()}Repository {
-  final ${featureName.capitalize()}RemoteDataSource remoteDataSource;
-
-  ${featureName.capitalize()}RepositoryImpl({required this.remoteDataSource});
-
-  @override
-  Future<Either<Exception, Unit>> callApi() async {
-    try {
-      return Right(await remoteDataSource.callApi());
-    } on Exception catch (exception) {
-      return Left(exception);
-    }
-  }
-
-}
-
-''');
-}
-
-void createDomainFiles(String featureName) {
-  File('lib/features/${featureName.toSnakeCase()}/domain/entities/${featureName.toSnakeCase()}.dart')
-      .writeAsStringSync('''
-class ${featureName.capitalize()} {
-  final String? data;
-
-  const ${featureName.capitalize()}({
-    required this.data,
-  });
-
-}
-''');
-  File('lib/features/${featureName.toSnakeCase()}/domain/repositories/${featureName.toSnakeCase()}_repository.dart')
-      .writeAsStringSync('''
-import 'package:dartz/dartz.dart';
-
-
-abstract class ${featureName.capitalize()}Repository {
-  Future<Either<Exception, Unit>> callApi();
-}
-
-''');
-  File('lib/features/${featureName.toSnakeCase()}/domain/use_cases/${featureName.toSnakeCase()}_use_case.dart')
-      .writeAsStringSync('''
-import 'package:dartz/dartz.dart';
-import '../repositories/${featureName.toSnakeCase()}_repository.dart';
-
-
-class ${featureName.capitalize()}UseCase {
-  final ${featureName.capitalize()}Repository repository;
-
-  ${featureName.capitalize()}UseCase(this.repository);
-
-  Future<Either<Exception, Unit>> call() async {
-    return await repository.callApi();
-  }
-}
-
-''');
-}
-
-void createPresentationFiles(String featureName) {
   File('lib/features/${featureName.toSnakeCase()}/presentation/bloc/${featureName.toSnakeCase()}_bloc.dart')
       .writeAsStringSync('''
 import '../../domain/use_cases/${featureName.toSnakeCase()}_use_case.dart';
@@ -188,16 +49,13 @@ class ${featureName.capitalize()}Bloc extends Bloc<${featureName.capitalize()}Ev
 
   ${featureName.capitalize()}Bloc({required this.${featureName}UseCase}) : super(${featureName.capitalize()}Initial());
 
-  @override
   Stream<${featureName.capitalize()}State> mapEventToState(${featureName.capitalize()}Event event) async* {
-    if (event is ${featureName.capitalize()}Event) {
-      yield ${featureName.capitalize()}Loading();
+      yield const ${featureName.capitalize()}Loading();
       final result = await ${featureName}UseCase.call();
       yield result.fold(
         (exception) => ${featureName.capitalize()}Error(exception),
-        (_) => ${featureName.capitalize()}Loaded(),
+        (_) => const ${featureName.capitalize()}Loaded(),
       );
-    }
   }
 }
 
@@ -290,11 +148,6 @@ class _${featureName.capitalize()}State extends State<${featureName.capitalize()
   }
 }
       ''');
-}
-
-void createInitParamsFile(String featureName) {
-  // File('lib/features/${featureName.toSnakeCase()}/${featureName.toSnakeCase()}_init_params.dart')
-  //     .createSync(recursive: true);
 }
 
 void createInjectionContainerFile(String featureName) {
